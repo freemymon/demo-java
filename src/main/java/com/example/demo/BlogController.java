78e65334ac10ee345e0e7105bf60b7df9b32a83e
freemymon <nalit.muangudom@gmail.com>
2021-08-13 11:11:21 +0700

    First Commit

---
+++ src/main/java/com/example/demo/BlogController.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BlogController {
+
+    @Autowired
+    BlogRespository blogRespository;
+
+    @GetMapping("/blog")
+    public List<Blog> index(){
+        return blogRespository.findAll();
+    }
+
+    @GetMapping("/blog/{id}")
+    public Blog show(@PathVariable String id){
+        Integer blogId = Integer.valueOf(id);
+        return blogRespository.findById(blogId).get();
+    }
+
+    @PostMapping("/blog/search")
+    public List<Blog> search(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return blogRespository.findByTitleContainingOrContentContaining(searchTerm, searchTerm);
+    }
+
+    @PostMapping("/blog")
+    public Blog create(@RequestBody Map<String, String> body){
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogRespository.save(new Blog(title, content));
+    }
+
+    @PutMapping("/blog/{id}")
+    public Blog update(@PathVariable String id, @RequestBody Map<String, String> body){
+        Integer blogId = Integer.valueOf(id);
+        // getting blog
+        Blog blog = blogRespository.findById(blogId).get();
+        blog.setTitle(body.get("title"));
+        blog.setContent(body.get("content"));
+        return blogRespository.save(blog);
+    }
+
+    @DeleteMapping("blog/{id}")
+    public boolean delete(@PathVariable String id){
+        Integer blogId = Integer.valueOf(id);
+        blogRespository.deleteById(blogId);
+        return true;
+    }
+
+
+}
+
